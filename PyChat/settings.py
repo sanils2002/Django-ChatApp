6bfd652ce78b765884cd58d8106407c80d26f10f
Sanil Sanjay Sattarkar <81878805+sanils2002@users.noreply.github.com>
2023-11-09 23:36:24 +0530

    Update settings.py

--- PyChat/settings.py
+++ PyChat/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "/"
